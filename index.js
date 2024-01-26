d6fca31c991b4e7eb19ec9a44d45cced0a45e3f5
Alicen Bender <alicene.bender@gmail.com>
2024-01-25 19:28:34 -0500

    Added array of questions for user input.

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const generateMarkdown = require('./generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "Add the project title:",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Add the project description:",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Add the Table of Contents (if applicable):",
+        name: "table",
+    },
+    {
+        type: "input",
+        message: "What are the steps needed install your project?",
+        name: "install",
+    },
+    {
+        type: "input",
+        message: "Instructions and examples of use:",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "Add any collaborators with the links to their GitHub profiles or any tutorials you used:",
+        name: "credits",
+    },
+    {
+        type: "input",
+        message: "Which license did you use?",
+        name: "license",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
